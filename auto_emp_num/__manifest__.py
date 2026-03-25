82015e4f3a98a23bdf6fc91eda7866e5b50babf5
brameodoo <brame.odoo@gmail.com>
2026-03-25 15:52:13 -0600

    Create __manifest__.py

---
+++ auto_emp_num/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Auto Employee Number & PIN',
+    'version': '19.0.1.0.0',
+    'category': 'Human Resources',
+    'depends': ['hr'],
+    'data': [
+        'data/ir_sequence_data.xml',
+        'views/hr_employee_views.xml',
+    ],
+    'installable': True,
+}
